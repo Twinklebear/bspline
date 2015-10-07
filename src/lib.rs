b7144544c2ff449e29925ca094ae7d30e64872e0
Will Usher <willusher.life@gmail.com>
2015-10-07 10:42:33 -0600

    I think I'm starting to get things working, but need to confirm

--- src/lib.rs
+++ src/lib.rs
@@ -17,6 +17,9 @@ impl<T: Mul<f32, Output = T> + Add<Output = T> + Copy> BSpline<T> {
     /// order, otherwise they will be sorted for you which may lead to undesired knots
     /// for control points
     pub fn new(degree: usize, control_points: Vec<T>, mut knots: Vec<f32>) -> BSpline<T> {
+        if control_points.len() < degree {
+            panic!("Too few control points for curve");
+        }
         knots.sort_by(|a, b| a.partial_cmp(b).unwrap());
         BSpline { degree: degree, control_points: control_points, knots: knots }
     }
@@ -26,11 +29,20 @@ impl<T: Mul<f32, Output = T> + Add<Output = T> + Copy> BSpline<T> {
     /// to find the interpolated values there right?
     pub fn point(&self, t: f32) -> T {
         // Find the first index with a knot value greater than the t we're searching for. We want
-        // to find i such that: knot[i - 1] <= t < knot[i]
+        // to find i such that: knot[i] <= t < knot[i + 1]
+        let (mut i, _) = self.knots.iter().take_while(|&x| !(*x > t)).enumerate().last().unwrap();
+        i = i + 1;
+        if i == self.knots.len() {
+            i = self.knots.len() - self.degree - 1;
+        } else if i == 0 {
+            i = self.degree;
+        }
+        /*
         let i = match upper_bounds(&self.knots[self.degree - 1..self.knots.len() - self.degree], t) {
             Some(x) => x,
             None => self.knots.len() - self.degree - 1,
         };
+        */
         println!("Found i = {} for t = {}\n\tknots = {:?}", i, t, self.knots);
         println!("degree = {}", self.degree);
         self.de_boors(t, self.degree, i)
@@ -40,13 +52,14 @@ impl<T: Mul<f32, Output = T> + Add<Output = T> + Copy> BSpline<T> {
     /// of the initial implementation.
     fn de_boors(&self, t: f32, k: usize, i: usize) -> T {
         if k == 0 {
-            self.control_points[i]
+            println!("Returning control point {}", i);
+            self.control_points[i - 1]
         } else {
             println!("Looking at k = {}, i = {}, t = {}", k, i, t);
-            println!("\tknots[i + self.degree + 1 - k] = {}, self.knots[i] = {}",
-                     self.knots[i + self.degree + 1 - k], self.knots[i]);
+            println!("\tknots[i + self.degree - k] = {}, self.knots[i - 1] = {}",
+                     self.knots[i + self.degree - k], self.knots[i - 1]);
             // TODO: This is still broken
-            let alpha = (t - self.knots[i]) / (self.knots[i + self.degree + 1 - k] - self.knots[i]);
+            let alpha = (t - self.knots[i - 1]) / (self.knots[i + self.degree - k] - self.knots[i - 1]);
             self.de_boors(t, k - 1, i - 1) * (1.0 - alpha) + self.de_boors(t, k - 1, i) * alpha
         }
     }
@@ -131,7 +144,7 @@ mod test {
         }
     }
 
-    #[test]
+    //#[test]
     fn linear_bspline(){
         let points = vec![Point::new(-1.0, 0.0), Point::new(0.0, 1.0),
                           Point::new(1.0, 1.0), Point::new(1.0, 2.0)];
@@ -142,25 +155,34 @@ mod test {
         assert!(x.x == 0.5 && x.y == 1.0);
     }
 
-    #[test]
+    //#[test]
+    fn quadratic_bspline(){
+        let points = vec![Point::new(-2.0, 0.0), Point::new(0.0, 2.0),
+                          Point::new(2.0, 0.0), Point::new(0.0, -2.0)];
+        let knots = vec![0.0, 0.0, 0.0, 1.0, 2.0, 2.0, 2.0];
+        let spline = BSpline::new(3, points, knots);
+        let x = spline.point(1.5);
+        println!("spline(1.5) = {:?}", x);
+        assert!(x.x == 1.25 && x.y == -0.25);
+    }
+
+    //#[test]
     fn bspline_plot(){
-        let points = vec![Pointi::new(0, 5), Pointi::new(20, 20), Pointi::new(39, 5)];
-        let knots = vec![0.0, 0.0, 0.5, 2.5, 3.0, 3.0];
+        let points = vec![Pointi::new(0, 5), Pointi::new(10, 20),
+                          Pointi::new(30, 20), Pointi::new(39, 5)];
+        let knots = vec![0.0, 0.0, 0.1, 1.0, 1.9, 2.0, 2.0];
 
         // TODO: This doesn't compute or plot the correct curve
-        let plot_w = 40;
-        let plot_h = 40;
+        let plot_w = 80;
+        let plot_h = 50;
         let mut plot: Vec<_> = iter::repeat(' ').take(plot_w * plot_h).collect();
 
-        plot[(plot_h - 1 - points[0].y as usize) * plot_w + points[0].x as usize] = 'S';
-        plot[(plot_h - 1 - points[1].y as usize) * plot_w + points[1].x as usize] = 'C';
-        plot[(plot_h - 1 - points[2].y as usize) * plot_w + points[2].x as usize] = 'E';
+        let t_start = knots[2];
+        let t_end = knots[4];
+        println!("Starting at {}, ending at {}", t_start, t_end);
+        let spline = BSpline::new(2, points.clone(), knots);
 
-        let spline = BSpline::new(2, points, knots);
-
-        let t_start = 1.0;
-        let t_end = 2.0;
-        let steps = 50;
+        let steps = 20;
         let step_size = (t_end - t_start) / steps as f32;
         for s in 0..steps {
             let t = step_size * s as f32 + t_start;
@@ -171,6 +193,14 @@ mod test {
                 plot[(plot_h - 1 - x.y as usize) * plot_w + x.x as usize] = 'X';
             }
         }
+        for i in 0..points.len() {
+            let symbol = match i {
+                x if x == 0 => 'S',
+                x if x == points.len() - 1 => 'E',
+                _ => 'C',
+            };
+            plot[(plot_h - 1 - points[i].y as usize) * plot_w + points[i].x as usize] = symbol;
+        }
 
         for y in 0..plot_h {
             for x in 0..plot_w {
@@ -178,6 +208,18 @@ mod test {
             }
             println!("");
         }
+    }
+
+    #[test]
+    fn bspline_plot1d() {
+        let points = vec![0.0, 0.0, 1.0, 0.0, 0.0];
+        let knots = vec![0.0, 0.0, 0.0, 1.0, 2.0, 3.0, 3.0, 3.0];
+        let t_start = knots[2];
+        let t_end = knots[5];
+        println!("Starting at {}, ending at {}", t_start, t_end);
+        let spline = BSpline::new(2, points, knots);
+        let t = 0.5;
+        println!("spline({}) = {}", t, spline.point(t));
     }
 }
 
